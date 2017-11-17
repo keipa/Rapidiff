b28226020914f31a3238bc00bec5390d48744dad
Rovdo, Nikolay <nichogaus@gmail.com>
2017-11-17 21:14:06 +0300

    Add project files.

---
+++ WindowsService1/ProjectInstaller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.RapiDiff = new System.ServiceProcess.ServiceInstaller();
+
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+
+
+            //Set the ServiceName of the Windows Service.
+            this.RapiDiff.ServiceName = "SimpleService";
+
+            //Set its StartType to Automatic.
+            this.RapiDiff.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+
+            //
+            // ProjectInstaller
+            //
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+                this.serviceProcessInstaller1,
+                this.RapiDiff});
+        }
+        private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+            using (System.ServiceProcess.ServiceController serviceController = new System.ServiceProcess.ServiceController(RapiDiff.ServiceName))
+            {
+                serviceController.Start();
+            }
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GlobalLowLevelHooks;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private KeyboardHook keyboardHook;
+        private void keyboardHook_KeyUp(KeyboardHook.VKeys key)
+        {
+            Debug.WriteLine("[" + DateTime.Now.ToLongTimeString() + "] KeyUp Event {" + key.ToString() + "}");
+        }
+        private void keyboardHook_KeyDown(KeyboardHook.VKeys key)
+        {
+            Debug.WriteLine("[" + DateTime.Now.ToLongTimeString() + "] KeyDown Event {" + key.ToString() + "}");
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            keyboardHook = new KeyboardHook();
+            keyboardHook.KeyDown += new KeyboardHook.KeyboardHookCallback(keyboardHook_KeyDown);
+            keyboardHook.KeyUp += new KeyboardHook.KeyboardHookCallback(keyboardHook_KeyUp);
+
+            //Installing the Keyboard Hooks
+            keyboardHook.Install();
+        }
+
+        private void end_Click(object sender, RoutedEventArgs e)
+        {
+            keyboardHook.KeyDown -= new KeyboardHook.KeyboardHookCallback(keyboardHook_KeyDown);
+            keyboardHook.KeyUp -= new KeyboardHook.KeyboardHookCallback(keyboardHook_KeyUp);
+            keyboardHook.Uninstall();
+        }
+    }
+}
